17224b9175db16a1df6e365bb5c1e153f83d8ce2
TriTruong666 <tritruonghoang3@gmail.com>
2026-04-09 00:25:47 +0700

    feat: deploy 1

--- app/core/config.py
+++ app/core/config.py
@@ -44,8 +44,8 @@ class Settings(BaseSettings):
     AUTH_PROVIDER: str = os.getenv("AUTH_PROVIDER")
     AUTH_GRPC_TARGET: str = os.getenv("AUTH_GRPC_TARGET")
     AUTH_GRPC_TIMEOUT_SECONDS: float = float(os.getenv("AUTH_GRPC_TIMEOUT_SECONDS", "2.0"))
-    
-    IS_RESET_DB: bool = os.getenv("IS_RESET_DB", "True").lower() in ("true", "1", "t")
+    # Quan trọng chỉ reset DB khi dev, prod khi cần thiết và thêm bảng hoặc sửa bảng
+    IS_RESET_DB: bool = os.getenv("IS_RESET_DB", "False").lower() in ("true", "1", "t")
 
 settings = Settings()
 
