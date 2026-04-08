5e8c6b476c16b3d5a5eab8baf42d467fcdb42475
TriTruong666 <tritruonghoang3@gmail.com>
2026-04-09 03:00:57 +0700

    feat: deploy 3

--- main.py
+++ main.py
@@ -84,8 +84,11 @@ def get_application() -> FastAPI:
     # Config CORS
     app.add_middleware(
         CORSMiddleware,
-        # allow_origins=["http://localhost:5173", "https://medimate.health.vn", "https://medimate.health.vn/", "https://medimate.health.vn/", "https://medimate.health.vn/"],
-        allow_origins=["*"],  # mốt nhớ đổi domain riel
+        allow_origins=[
+            "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "https://medimate.health.vn",
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
